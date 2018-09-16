79cc2e3c0c3e96d1c99db69d03ebacc2edd4e4c5
Andraz <pika.andraz.kralj@gmail.com>
2018-09-16 19:25:29 +0200

    Enemy Spawning, Difficulty and RNG Initial

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public float speed;
+
+    // Use this for initialization
+    void Start() {
+        if (transform.position.x > 0)
+        {
+            speed *= -1;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit player");
+        }
+        else if (other.gameObject.tag == "Animation")
+        {
+            Debug.Log("Start punching animation");
+        }
+    }
 }
---
+++ Assets/Scripts/Enemy/EnemySpawnerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerController : MonoBehaviour {
+
+    public GameObject enemy1;
+    public GameObject enemy2;
+
+    public int enemy1Ratio;
+    public int enemy2Ratio;
+
+    public float spawnTime;
+    public float spawnTimeChange;
+
+    public float enemySpeed;
+    public float enemySpeedChange;
+
+    public int diffucultyInterval;
+
+    private int count = 0;
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime
+        Invoke("Spawn", spawnTime - Random.Range(0f, spawnTimeChange));
+    }
+
+
+    void Spawn()
+    {
+        // Create an instance of the enemy prefab at the spawn point's position
+        GameObject clone;
+        float random = Random.Range(1f, enemy1Ratio + enemy2Ratio);
+        if(random <= enemy1Ratio)
+        {
+            clone = Instantiate(enemy1, transform.position, transform.rotation);
+        }
+        else /* if (random <= enemy1Ratio + enemy2Ratio) */
+        {
+            clone = Instantiate(enemy2, transform.position, transform.rotation);
+        }
+
+        // Get controller of spawned enemy
+        EnemyController cloneController = clone.GetComponent<EnemyController>();
+
+        // Do things to enemy (speed, different sprites and hp and etc.)
+        cloneController.speed = enemySpeed + Random.Range(0f, enemySpeedChange);
+        // Increment counter for spawns
+        count++;
+
+        // Increase difficulty after a certain amount of enemies have spawned
+        if (count % diffucultyInterval == 0)
+        {
+            spawnTime -= spawnTimeChange;
+            // Cap spawn time and 0.15s
+            if (spawnTime < 0.15)
+                spawnTime = 0.15f;
+            // Cap enemy speed at 0.3
+            enemySpeed += enemySpeedChange;
+            if (enemySpeed > 0.3)
+                enemySpeed = 0.3f;
+        }
+
+        // Call the Spawn function after a delay of the spawnTime.
+        Invoke("Spawn", spawnTime - Random.Range(0f, spawnTimeChange));
+    }
+}
